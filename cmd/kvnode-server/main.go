ece64ddea0e905ed323156396be583468d8ba64f
Josh Baker <joshbaker77@gmail.com>
2017-01-31 07:44:43 -0700

    use full address

--- cmd/kvnode-server/main.go
+++ cmd/kvnode-server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 
 	"github.com/tidwall/redlog"
@@ -10,15 +9,14 @@ import (
 )
 
 func main() {
-	var port int
+	var addr string
 	var data string
 	var join string
-	flag.IntVar(&port, "p", 4920, "server port")
-	flag.StringVar(&data, "d", "data", "data directory")
+	flag.StringVar(&addr, "addr", "127.0.0.1:4920", "bind/discoverable ip:port")
+	flag.StringVar(&data, "data", "data", "data directory")
 	flag.StringVar(&join, "join", "", "Join a cluster by providing an address")
 	flag.Parse()
 	var log = redlog.New(os.Stderr)
-	addr := fmt.Sprintf(":%d", port)
 	if err := node.ListenAndServe(addr, join, data); err != nil {
 		log.Warningf("%v", err)
 	}
